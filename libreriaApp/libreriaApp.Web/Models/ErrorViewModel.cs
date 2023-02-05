d3d7d07533c304e5b8c66edadef5a41d8b235701
Albieri Garcia <albieri404@gmail.com>
2023-02-04 20:42:32 -0400

    Primera subida

---
+++ libreriaApp/libreriaApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace libreriaApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
