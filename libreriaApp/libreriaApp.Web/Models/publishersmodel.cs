7c49cf73add3ec34987d043f8e7351852301594c
Janiel Flores Almanzar <Janielfloresdo@gmail.com>
2023-02-11 11:05:55 -0400

    subida de codigo

--- libreriaApp/libreriaApp.Web/Models/publishersmodel.cs
+++ libreriaApp/libreriaApp.Web/Models/publishersmodel.cs
@@ -6,7 +6,6 @@
         public string Name { get; set; }    
         public string City { get; set; }
         public string State { get; set; }
-
         public string Country { get; set; }
             
 
